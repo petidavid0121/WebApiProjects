26eec1c1764e254c2eefc6fb74aeecbbe0b028e6
petidavid0121 <peter.david2@stud.uni-corvinus.hu>
2023-11-28 10:22:54 +0100

    a

---
+++ WebAPIBase/wwwroot/geek.js
@@ -0,0 +1,21 @@
+﻿var viccek;
+
+window.onload = letöltés;
+function letöltés() {
+    fetch('/jokes.json')
+        .then(response => response.json())
+        .then(data => letöltésBefejeződött(data)
+    );
+}
+
+function letöltésBefejeződött(k) {
+    console.log("sikeres letöltés")
+    console.log(k)
+    viccek = k;
+    for (var i = 0; i < k.length ; i++)
+    {
+        var ujElem = document.createElement('div')
+        ujElem.innerText = k[i]
+        document.getElementById('main').appendChild(ujElem)
+    }
+}
