ceb2910bff94220d23f89ba030a63fc6aee6fd35
petidavid0121 <peter.david2@stud.uni-corvinus.hu>
2023-11-28 11:05:51 +0100

    kerdesek oldal done I guess

---
+++ WebAPIBase/Controllers/BoatController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPIBase.Controllers
+{
+    public class BoatController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebAPIBase/Controllers/TestController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPIBase.Controllers
+{
+    [ApiController]
+    public class TestController : Controller
+    {
+        [HttpGet]
+        [Route("corvinus/szerverido")]
+        public IActionResult M1()
+        {
+            string pontosIdő = DateTime.Now.ToShortTimeString();
+
+            return new ContentResult
+            {
+                ContentType = System.Net.Mime.MediaTypeNames.Text.Plain, //"text/plain"
+                Content = pontosIdő
+            };
+        }
+    }
+}
